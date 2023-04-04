6ecf682e60f324e617ec53fca5414e861c058ec4
darthMDMA <basalayva004@mail.ru>
2023-04-04 17:02:15 +0300

    Проверка страницы на добавление в корзину и алертов

--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,15 @@
 from selenium.webdriver.common.by import By
+from .product_page import *
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class AddProductLocators:
+    ADD_PRODUCT = (By.CLASS_NAME, "btn-add-to-basket")
+    PRICE = (By.XPATH, '//p[@class="price_color"]')
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,16 @@
 from .base_page import BasePage
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
-from .pages.locators import MainPageLocators
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+
